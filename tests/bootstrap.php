e8f1ff31ca8f3c7b632dd77ce514562530a74982
mm <eyou80@gmail.com>
2016-01-20 21:11:54 +0800

    tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,13 @@
 <?php
 
+error_reporting(E_ALL);
 
+if (function_exists('date_default_timezone_set') && function_exists('date_default_timezone_get')) {
+    date_default_timezone_set(@date_default_timezone_get());
+}
+
+require __DIR__ . '/Wee/TestCase.php';
+
+// autoload
+$loader = require __DIR__ . '/../vendor/autoload.php';
+// $loader->add('Wee\\Test\\', __DIR__);
